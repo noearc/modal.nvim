555e207a91425408390cffe0bc5078ecaaaa5ead
n451 <2020200706@ruc.edu.cn>
2024-06-03 00:28:09 +0800

    working on the new structure, ports are issue...

--- lua/modal-nvim.lua
+++ lua/modal-nvim.lua
@@ -1,6 +1,5 @@
 local M = {}
--- require("moon.all")
-local yue = require("yue")
+local Job = require("plenary.job")
 
 local DEFAULTS = {
 	boot = {
@@ -15,19 +14,20 @@ local DEFAULTS = {
 		-- split = "v",
 	},
 	keymaps = {
-		send_line = "<C-l>",
-		send_node = "<Leader>s",
-		send_visual = "<C-l>",
-		hush = "<C-H>",
+		send_line = "<leader>l",
+		-- send_node = "<Leader>s",
+		send_block = "<leader>e",
+		send_visual = "<leader>l",
+		hush = "<leader>.",
 	},
 }
 
 local KEYMAPS = {
-	-- 	send_line = {
-	-- 		mode = "n",
-	-- 		action = "Vy<cmd>lua require('tidal').send_reg()<CR><ESC>",
-	-- 		description = "send line to Tidal",
-	-- 	},
+	send_line = {
+		mode = "n",
+		action = "Vy<cmd>lua require('modal-nvim').send_reg()<CR><ESC>",
+		description = "send line to Tidal",
+	},
 	-- 	send_node = {
 	-- 		mode = "n",
 	-- 		action = function()
@@ -35,18 +35,25 @@ local KEYMAPS = {
 	-- 		end,
 	-- 		description = "send treesitter node to Tidal",
 	-- 	},
+	send_block = {
+		mode = "n",
+		action = function()
+			M.send_block()
+		end,
+		description = "send block to Tidal",
+	},
 	send_visual = {
 		mode = "v",
 		action = "y<cmd>lua require('nvim-modal').send_reg()<CR>",
 		description = "send selection to Tidal",
 	},
-	-- 	hush = {
-	-- 		mode = "n",
-	-- 		action = function()
-	-- 			T.send("hush")
-	-- 		end,
-	-- 		description = "send 'hush' to Tidal",
-	-- 	},
+	hush = {
+		mode = "n",
+		action = function()
+			M.send("hush!")
+		end,
+		description = "send 'hush' to Tidal",
+	},
 }
 
 local state = {
@@ -57,19 +64,18 @@ local state = {
 	sclang_process = nil,
 }
 
-local function send(text)
-	print(text)
-	if not state.modal_process then
-		print("no proc")
-		return
-	end
-	text = yue.to_lua(text)
-	vim.api.nvim_chan_send(state.modal_process, text .. "\n")
+function M.send(text)
+	-- if not state.modal_process then
+	-- 	print("no proc")
+	-- 	return
+	-- end
+	-- text = yue.to_lua(text)
+	vim.api.nvim_chan_send(state.repl_process, text .. "\n")
 end
 
 function M.send_current_line()
 	local l = vim.api.nvim_get_current_line()
-	send(l)
+	M.send(l)
 end
 
 local update_lines = function()
@@ -95,15 +101,15 @@ function M.send_block()
 	for i, v in ipairs(sections) do
 		local res = ""
 		if cursor[1] <= v then
-			res = table.concat(vim.api.nvim_buf_get_lines(0, sections[i - 1], sections[i], false), "\n")
-			send(res)
+			res = table.concat(vim.api.nvim_buf_get_lines(0, sections[i - 1], sections[i], false), "")
+			M.send(res)
 			break
 		end
 	end
 end
 
 function M.send_all()
-	send(table.concat(Lines, "\n"))
+	M.send(table.concat(Lines, "\n"))
 end
 
 -- TODO: send visual
@@ -113,7 +119,7 @@ function M.send_reg(register)
 		register = ""
 	end
 	local text = table.concat(vim.fn.getreg(register, 1, true), "\n")
-	send(text)
+	M.send(text)
 end
 
 local function boot_modal(args)
@@ -129,14 +135,34 @@ local function boot_modal(args)
 		boot_modal(args)
 		return
 	end
-	state.modal_process = vim.fn.termopen("modal", {
+	-- state.modal_process = vim.fn.termopen("mods", {
+	-- 	on_exit = function()
+	-- 		if #vim.fn.win_findbuf(state.modal) > 0 then
+	-- 			vim.api.nvim_win_close(vim.fn.win_findbuf(state.modal)[1], true)
+	-- 		end
+	-- 		vim.api.nvim_buf_delete(state.modal, {})
+	-- 		state.modal = nil
+	-- 		state.modal_process = nil
+	-- 	end,
+	-- })
+	state.modal_process = Job:new({
+		command = "mods",
+		args = { 9000 }, -- FIND avail
+		on_exit = function()
+			print("server quit!")
+		end,
+	})
+	print(vim.inspect(state.modal_process))
+	state.modal_process:start()
+
+	state.repl_process = vim.fn.termopen("modal", {
 		on_exit = function()
 			if #vim.fn.win_findbuf(state.modal) > 0 then
 				vim.api.nvim_win_close(vim.fn.win_findbuf(state.modal)[1], true)
 			end
 			vim.api.nvim_buf_delete(state.modal, {})
 			state.modal = nil
-			state.modal_process = nil
+			state.modal_process:shutdown()
 		end,
 	})
 end
@@ -172,8 +198,8 @@ local function launch_modal(args)
 	end
 	vim.cmd(args.split == "v" and "vsplit" or "split")
 	boot_modal(args.modal)
-	vim.cmd(args.split == "v" and "split" or "vsplit")
-	boot_sclang(args.sclang)
+	-- vim.cmd(args.split == "v" and "split" or "vsplit")
+	-- boot_sclang(args.sclang)
 	vim.api.nvim_set_current_win(current_win)
 	state.launched = true
 	Lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
@@ -184,8 +210,9 @@ local function exit()
 	if not state.launched then
 		return
 	end
-	if state.tidal_process then
-		vim.fn.jobstop(state.modal_process)
+	if state.modal_process then
+		-- vim.fn.jobstop(state.modal_process)
+		state.modal_process:shutdown()
 	end
 	if state.sclang_process then
 		vim.fn.jobstop(state.sclang_process)
@@ -208,14 +235,23 @@ function M.setup(args)
 	vim.api.nvim_create_user_command("ModalLaunch", function()
 		launch_modal(args.boot)
 	end, { desc = "launches Modal instance, including sclang if so configured" })
-	vim.api.nvim_create_user_command("ModalExit", exit, { desc = "quits modal instance" })
+	vim.api.nvim_create_user_command("ModalExit", function()
+		exit()
+	end, { desc = "quits modal instance" })
 	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 		pattern = { "*.modal" },
 		callback = function()
-			vim.cmd("set ft=yue")
+			-- vim.cmd("set ft=yue")
 			for key, value in pairs(args.keymaps) do
 				key_map(key, value)
 			end
+		end,
+	})
+
+	vim.api.nvim_create_autocmd("VimLeave", {
+		pattern = { "*.modal" },
+		callback = function()
+			exit()
 		end,
 	})
 end
